ecebdd96a2d08670aa646c7656d60ddd47b9cbc7
xncbf <xncbf12@gmail.com>
2016-06-11 01:41:40 +0900

    change url detail

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^blog/(?P<current_paging_number>)$', views.blog, name='blog'),
     url(r'^blog/(?P<current_paging_number>[0-9]+)/$', views.blog, name='blog'),
-    url(r'^detail/(?P<board_number>[0-9]+)/$', views.blog_detail, name='blog'),
+    url(r'^blog/detail/(?P<board_number>[0-9]+)/$', views.blog_detail, name='blog'),
 ]
