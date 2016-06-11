8770de4dc91c46f313e984097766949f868111bd
xncbf <xncbf12@gmail.com>
2016-06-11 22:57:57 +0900

    edit urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^blog/(?P<current_paging_number>)$', views.blog, name='blog'),
-    url(r'^blog/(?P<current_paging_number>[0-9]+)/$', views.blog, name='blog'),
+    url(r'^blog/(?P<current_paging_number>)(?P<category>)$', views.blog, name='blog'),
+    url(r'^blog/(?P<current_paging_number>[0-9]+)(?P<category>)/$', views.blog, name='blog'),
     url(r'^blog/detail/(?P<board_number>[0-9]+)/$', views.blog_detail, name='blog'),
+    url(r'^blog/(?P<category>[0-9]+)/(?P<current_paging_number>[0-9]+)/$', views.blog, name='blog'),
 ]
