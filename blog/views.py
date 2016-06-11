d113d29db3d7266553102755a5515284e899a8eb
xncbf <xncbf12@gmail.com>
2016-06-11 22:58:06 +0900

    add Category

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Gallery, Image
+from .models import Gallery, Image, Categorys
 from django.core.paginator import Paginator
 
 
@@ -10,15 +10,18 @@ def index(request):
     })
 
 
-def blog(request, current_paging_number):
+def blog(request, current_paging_number, category):
     if current_paging_number == '':
         current_paging_number = '1'
-
-    page = Paginator(Gallery.objects.order_by('-created_date'), 5)
+    if category == '':
+        page = Paginator(Gallery.objects.order_by('-created_date'), 5)
+    else:
+        page = Paginator(Gallery.objects.filter(categorys=category).order_by('-created_date'), 5)
     page_count = 4
     page_max_count = page._get_num_pages()
     get_gallery = page
     get_image = Image.objects.filter(thumbnail='True')
+    get_category = Categorys.objects.all()
     if (page_max_count-1) // page_count == (int(current_paging_number)-1) // page_count:
         page_loop = page_max_count % page_count
     else:
@@ -30,14 +33,18 @@ def blog(request, current_paging_number):
         'page_loop': page_loop,
         'page_size': page_count,
         'page_max_count': page_max_count,
+        'category_list': get_category,
+        'category': category,
     })
 
 
 def blog_detail(request, board_number):
     get_blog_detail = Gallery.objects.filter(id=board_number)[0]
     get_image = Image.objects.filter(gallery=board_number)
+    get_category = Categorys.objects.all()
     return render(request, 'cluster/blog_detail.html', {
         'board_number': board_number,
         'blog': get_blog_detail,
         'image_list': get_image,
+        'category_list': get_category,
     })
