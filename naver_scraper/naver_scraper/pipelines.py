b6e6d3e3ab18eda7ec7d662f7a98387bb715eee1
xncbf <xncbf12@gmail.com>
2016-06-20 15:02:47 +0900

    test

--- naver_scraper/naver_scraper/pipelines.py
+++ naver_scraper/naver_scraper/pipelines.py
@@ -4,20 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import re
+
 
 class NaverScraperPipeline(object):
     def __init__(self):
         self.ids_seen = set()
+
     #몰라 예제에있던거
     def process_item(self, item, spider):
-        item['address'] = self.cleanup_address(item['address'])
         item.save()
         return item
 
     #중복제거래
-    def process_item(self, item, spider):
-        if item['id'] in self.ids_seen:
-            raise DropItem("Duplicate item found: %s" % item)
-        else:
-            self.ids_seen.add(item['id'])
-            return item
+    # def process_item(self, item, spider):
+    #     if item['id'] in self.ids_seen:
+    #         raise DropItem("Duplicate item found: %s" % item)
+    #     else:
+    #         self.ids_seen.add(item['id'])
+    #         return item
--- naver_scraper/naver_scraper/spiders/naver_scraper_spider.py
+++ naver_scraper/naver_scraper/spiders/naver_scraper_spider.py
@@ -1,27 +1,46 @@
 import scrapy
+from scrapy.selector import Selector
 from naver_scraper.items import NaverScraperItem
 
 class NaverBlogSpider(scrapy.Spider):
     name = "naverblog"
     allowed_domains = ["naver.com"]
     start_urls = [
-        "http://blog.naver.com/kdk926/PostList.nhn?from=postList&blogId=kdk926&currentPage=9999",
+        "http://blog.naver.com/kdk926/PostList.nhn?from=postList&blogId=kdk926&currentPage=1",
+        # "http://blog.naver.com/kdk926/PostList.nhn?from=postList&blogId=kdk926&currentPage=9999",
     ]
 
     def parse(self, response):
         # find form and fill in
         # call inner parse to parse real results.
-        num = int(response.xpath("//table[@class='page-navigation']/tr/td[@class='cnt']/a/text()").extract()[-1])
-        for idx in range(1, num):
-            request = scrapy.FormRequest("http://blog.naver.com/kdk926/PostList.nhn?from=postList&blogId=kdk926&currentPage="+ str(idx),
-                                         callback=self.parse_blog)
-            yield request
+        hxs = Selector(response)
+        selects = hxs.xpath("//td[@class='bcc']")
+        naverscraperItem = []
+        for e in selects:
+            naverscraperItem = NaverScraperItem()
+            naverscraperItem['title'] = e.xpath("..//h3/text()[2]").extract()[0]
+            naverscraperItem['detail'] = e.xpath("..//div[@class='se_component_wrap sect_dsc __se_component_area']").extract()[0]
+            naverscraperItem['created_date'] = e.xpath("..//span[@class='se_publishDate pcol2 fil5']/text()").extract()[0]
+            # naverscraperItem['categorys.name'] = e.xpath("..//div[@class='se_series']/*/text()").extract()[0]
+            naverscraperItem['categorys_id'] = '1'
+            naverscraperItem.append(naverscraperItem) #Item 1개 세트를 리스트에 담음
+        return naverscraperItem
 
-    def parse_blog(self, response):
-        for e in response.xpath("//*[@class='resultadosNumeroSuc']"):
-                naverscraperItem = NaverScraperItem()
-                naverscraperItem['title'] = e.xpath("../*[@class='resultadosTextWhite']/text()").extract()[0]
-                naverscraperItem['detail'] = e.xpath("../*[@class='resultadosTextWhite']/text()").extract()[0]
-                naverscraperItem['created_date'] = e.xpath("../*[@class='resultadosTextWhite']/text()").extract()[0]
-                naverscraperItem['categorys.name'] = e.xpath("../*[@class='resultadosTextWhite']/text()").extract()[0]
-        yield naverscraperItem
+    # def parse(self, response):
+    #     # find form and fill in
+    #     # call inner parse to parse real results.
+    #     num = int(response.xpath("//table[@class='page-navigation']/tr/td[@class='cnt']/a/text()").extract()[-1])
+    #     for idx in range(1, num):
+    #         request = scrapy.FormRequest("http://blog.naver.com/kdk926/PostList.nhn?from=postList&blogId=kdk926&currentPage="+ str(idx),
+    #                                      callback=self.parse_blog)
+    #         yield request
+    #
+    # def parse_blog(self, response):
+    #     for idx, e in response.xpath("//td[@class='bcc']"):
+    #         naverscraperItem = NaverScraperItem()
+    #         naverscraperItem['title'] = e.xpath("..//h3/text()[2]").extract()[0]
+    #         naverscraperItem['detail'] = e.xpath("..//div[@class='se_component_wrap sect_dsc __se_component_area']").extract()[0]
+    #         naverscraperItem['created_date'] = e.xpath("..//span[@class='se_publishDate pcol2 fil5']/text()").extract()[0]
+    #         # naverscraperItem['categorys.name'] = e.xpath("..//div[@class='se_series']/*/text()").extract()[0]
+    #         naverscraperItem['categorys_id'] = '1'
+    #         yield naverscraperItem
