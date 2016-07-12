eacb9e76f6a2a785507910135d5025b5830265e9
xncbf <xncbf12@gmail.com>
2016-07-13 01:23:16 +0900

    haystack 테스트

--- aquam/blog/search_indexes.py
+++ aquam/blog/search_indexes.py
@@ -7,10 +7,12 @@ class GalleryIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     pub_date = indexes.DateTimeField(model_attr='created_date')
 
+    content_auto = indexes.EdgeNgramField(model_attr='title')
+
 
     def get_model(self):
         return Gallery
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
+        return self.get_model().objects.all()
